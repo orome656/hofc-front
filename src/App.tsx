6a725c2b4354be59c05d898e1230c6e7c587daa1
Anthony Maladot <anthony.maladot@netc.fr>
2021-05-17 23:36:26 +0200

    app style

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header onViewChange={setView} />
+      <Header currentView={view} onViewChange={setView} />
       <Main currentView={view}/>
     </div>
   );
--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -1,12 +1,20 @@
 class HeaderProps {
+    currentView: string
     onViewChange: Function
 }
 
 function Header(props: HeaderProps) {
 
+    var computeLinkClass = function(currentView: string, viewName: string) {
+        if (currentView === viewName) {
+            return "active"
+        }
+        return ""
+    }
+
     return <header>
-        <a onClick={() => props.onViewChange("classement")}>Classement</a>
-        <a onClick={() => props.onViewChange("calendrier")}>Calendrier</a>
+        <a className={computeLinkClass(props.currentView, "classement")} onClick={() => props.onViewChange("classement")}>Classement</a>
+        <a className={computeLinkClass(props.currentView, "calendrier")} onClick={() => props.onViewChange("calendrier")}>Calendrier</a>
     </header>
 }
 
