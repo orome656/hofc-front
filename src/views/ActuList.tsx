2e0093cebf18f9c4f1892dc823bc26852c5f8377
Anthony Maladot <anthony.maladot@netc.fr>
2021-07-06 23:24:42 +0200

    view improvments

--- src/views/ActuList.tsx
+++ src/views/ActuList.tsx
@@ -1,25 +1,20 @@
 import { Actu } from '../models/Actu'
-import { makeStyles } from '@material-ui/core/styles'
 import ActuListItem from './ActuListItem';
+import Grid from '@material-ui/core/Grid';
 
 class ActuListProps {
     actus: Array<Actu>
 }
 
-
-const useStyles = makeStyles({
-    root: {
-      display: "flex",
-      "flex-wrap": "wrap"
-    },
-  });
-
 function ActuList(props: ActuListProps) {
-    const classes = useStyles();
-
-    return <div className={classes.root}>
-        {props.actus.map((item, index) => <ActuListItem actu={item}/>)}
-    </div>
+    return <Grid
+        container
+        spacing={3}
+        direction="row"
+        justify="center"
+        alignItems="center">
+        {props.actus.map((item, index) => <Grid item ><ActuListItem actu={item}/></Grid>)}
+    </Grid>
 }
 
 export default ActuList
