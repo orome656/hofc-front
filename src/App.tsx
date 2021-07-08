9b9e918515281b695f901986226e6287744b117f
Anthony Maladot <anthony.maladot@netc.fr>
2021-07-08 22:26:34 +0200

    Use app drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,159 @@
 import { useState } from 'react';
 import './App.css';
 import Main from './views/Main';
-import Header from './views/Header';
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: 0,
+    },
+  }),
+);
+
 
 function App() {
-  const [view, setView] = useState("classement")
+  const [view, setView] = useState("actualites")
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-  return (
-    <div className="App">
-      <Header currentView={view} onViewChange={setView} />
-      <Main currentView={view}/>
-    </div>
-  );
+  const handleDrawerOpen = () => {
+      setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+      setOpen(false);
+  };
+
+  const handleChange = (newValue: string) => {
+      setView(newValue);
+      setOpen(false)
+  };
+  
+  return <div>
+    <CssBaseline />
+    <AppBar
+      position="fixed"
+      className={clsx(classes.appBar, {
+        [classes.appBarShift]: open,
+      })}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          className={clsx(classes.menuButton, open && classes.hide)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap>
+          HOFC
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={open}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </IconButton>
+      </div>
+      <Divider />
+      <List>
+          <ListItem button onClick={() => handleChange("actualites")} key="actualites">
+            <ListItemIcon><InboxIcon /></ListItemIcon>
+            <ListItemText primary="Actualités" />
+          </ListItem>
+          <ListItem button onClick={() => handleChange("classement")} key="classement">
+            <ListItemIcon><InboxIcon /></ListItemIcon>
+            <ListItemText primary="Classements" />
+          </ListItem>
+          <ListItem button onClick={() => handleChange("calendrier")} key="calendrier">
+            <ListItemIcon><InboxIcon /></ListItemIcon>
+            <ListItemText primary="Calendrier" />
+          </ListItem>
+      </List>
+    </Drawer>
+    <main
+        className={clsx(classes.content)}
+      >
+        <div className={classes.drawerHeader} />
+        <Main currentView={view}/>
+      </main>
+  </div>
 }
 
 export default App;
